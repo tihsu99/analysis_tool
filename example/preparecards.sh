17f66d1811920e7aee35640be69f6cec6d23118c
zhenggan <zheng-gang.chen@cern.ch>
2022-10-28 19:23:56 +0200

    [Add file] example/preparecards.sh -> preparecards for all rtu samples

--- example/preparecards.sh
+++ example/preparecards.sh
@@ -25,53 +25,106 @@ python prepareCards.py -y run2 -c ee --For template
 python prepareCards.py -y run2 -c em --For template
 python prepareCards.py -y run2 -c mm --For template
 #rtu0p4
+#2016apv
 python prepareCards.py -y 2016apv -c em -reg 'SR_em' --For specific --coupling_value rtu0p4 --Masses 200 300 350 400 500 600 700 800 900 1000 ;
 python prepareCards.py -y 2016apv -c ee -reg 'SR_ee' --For specific --coupling_value rtu0p4 --Masses 200 300 350 400 500 600 700 800 900 1000 ;
 python prepareCards.py -y 2016apv -c mm -reg 'SR_mm' --For specific --coupling_value rtu0p4 --Masses 200 300 350 400 500 600 700 800 900 1000 ;
 python prepareCards.py -y 2016apv -c C  --For specific --coupling_value rtu0p4 --Masses 200 300 350 400 500 600 700 800 900 1000 ;
-
+#2016postapv
 python prepareCards.py -y 2016postapv -c em -reg 'SR_em' --For specific --coupling_value rtu0p4 --Masses 200 300 350 400 500 600 700 800 900 1000 ;
 python prepareCards.py -y 2016postapv -c ee -reg 'SR_ee' --For specific --coupling_value rtu0p4 --Masses 200 300 350 400 500 600 700 800 900 1000 ;
 python prepareCards.py -y 2016postapv -c mm -reg 'SR_mm' --For specific --coupling_value rtu0p4 --Masses 200 300 350 400 500 600 700 800 900 1000 ;
 python prepareCards.py -y 2016postapv -c C  --For specific --coupling_value rtu0p4 --Masses 200 300 350 400 500 600 700 800 900 1000 ;
-
+#2017
 python prepareCards.py -y 2017 -c em -reg 'SR_em' --For specific --coupling_value rtu0p4 --Masses 200 300 350 400 500 600 700 800 900 1000 ;
 python prepareCards.py -y 2017 -c ee -reg 'SR_ee' --For specific --coupling_value rtu0p4 --Masses 200 300 350 400 500 600 700 800 900 1000 ;
 python prepareCards.py -y 2017 -c mm -reg 'SR_mm' --For specific --coupling_value rtu0p4 --Masses 200 300 350 400 500 600 700 800 900 1000 ;
 python prepareCards.py -y 2017 -c C  --For specific --coupling_value rtu0p4 --Masses 200 300 350 400 500 600 700 800 900 1000 ;
-
+#2018
 python prepareCards.py -y 2018 -c em -reg 'SR_em' --For specific --coupling_value rtu0p4 --Masses 200 300 350 400 500 600 700 800 900 1000 ;
 python prepareCards.py -y 2018 -c ee -reg 'SR_ee' --For specific --coupling_value rtu0p4 --Masses 200 300 350 400 500 600 700 800 900 1000 ;
 python prepareCards.py -y 2018 -c mm -reg 'SR_mm' --For specific --coupling_value rtu0p4 --Masses 200 300 350 400 500 600 700 800 900 1000 ;
 python prepareCards.py -y 2018 -c C  --For specific --coupling_value rtu0p4 --Masses 200 300 350 400 500 600 700 800 900 1000 ; 
-
+#run2
 python prepareCards.py -y run2 -c em -reg 'SR_em' --For specific --coupling_value rtu0p4 --Masses 200 300 350 400 500 600 700 800 900 1000 ;
 python prepareCards.py -y run2 -c ee -reg 'SR_ee' --For specific --coupling_value rtu0p4 --Masses 200 300 350 400 500 600 700 800 900 1000 ;
 python prepareCards.py -y run2 -c mm -reg 'SR_mm' --For specific --coupling_value rtu0p4 --Masses 200 300 350 400 500 600 700 800 900 1000 ;
 python prepareCards.py -y run2 -c C  --For specific --coupling_value rtu0p4 --Masses 200 300 350 400 500 600 700 800 900 1000 ;
-#rtu0p4
-python prepareCards.py -y 2016apv -c em -reg 'SR_em' --For specific --coupling_value rtc0p4 --Masses 200 300 350 400 500 600 700 800 900 1000 ;
-python prepareCards.py -y 2016apv -c ee -reg 'SR_ee' --For specific --coupling_value rtc0p4 --Masses 200 300 350 400 500 600 700 800 900 1000 ;
-python prepareCards.py -y 2016apv -c mm -reg 'SR_mm' --For specific --coupling_value rtc0p4 --Masses 200 300 350 400 500 600 700 800 900 1000 ;
-python prepareCards.py -y 2016apv -c C  --For specific --coupling_value rtc0p4 --Masses 200 300 350 400 500 600 700 800 900 1000 ;
-
-python prepareCards.py -y 2016postapv -c em -reg 'SR_em' --For specific --coupling_value rtc0p4 --Masses 200 300 350 400 500 600 700 800 900 1000 ;
-python prepareCards.py -y 2016postapv -c ee -reg 'SR_ee' --For specific --coupling_value rtc0p4 --Masses 200 300 350 400 500 600 700 800 900 1000 ;
-python prepareCards.py -y 2016postapv -c mm -reg 'SR_mm' --For specific --coupling_value rtc0p4 --Masses 200 300 350 400 500 600 700 800 900 1000 ;
-python prepareCards.py -y 2016postapv -c C  --For specific --coupling_value rtc0p4 --Masses 200 300 350 400 500 600 700 800 900 1000 ;
-
-python prepareCards.py -y 2017 -c em -reg 'SR_em' --For specific --coupling_value rtc0p4 --Masses 200 300 350 400 500 600 700 800 900 1000 ;
-python prepareCards.py -y 2017 -c ee -reg 'SR_ee' --For specific --coupling_value rtc0p4 --Masses 200 300 350 400 500 600 700 800 900 1000 ;
-python prepareCards.py -y 2017 -c mm -reg 'SR_mm' --For specific --coupling_value rtc0p4 --Masses 200 300 350 400 500 600 700 800 900 1000 ;
-python prepareCards.py -y 2017 -c C  --For specific --coupling_value rtc0p4 --Masses 200 300 350 400 500 600 700 800 900 1000 ;
-
-python prepareCards.py -y 2018 -c em -reg 'SR_em' --For specific --coupling_value rtc0p4 --Masses 200 300 350 400 500 600 700 800 900 1000 ;
-python prepareCards.py -y 2018 -c ee -reg 'SR_ee' --For specific --coupling_value rtc0p4 --Masses 200 300 350 400 500 600 700 800 900 1000 ;
-python prepareCards.py -y 2018 -c mm -reg 'SR_mm' --For specific --coupling_value rtc0p4 --Masses 200 300 350 400 500 600 700 800 900 1000 ;
-python prepareCards.py -y 2018 -c C  --For specific --coupling_value rtc0p4 --Masses 200 300 350 400 500 600 700 800 900 1000 ; 
-
-python prepareCards.py -y run2 -c em -reg 'SR_em' --For specific --coupling_value rtc0p4 --Masses 200 300 350 400 500 600 700 800 900 1000 ;
-python prepareCards.py -y run2 -c ee -reg 'SR_ee' --For specific --coupling_value rtc0p4 --Masses 200 300 350 400 500 600 700 800 900 1000 ;
-python prepareCards.py -y run2 -c mm -reg 'SR_mm' --For specific --coupling_value rtc0p4 --Masses 200 300 350 400 500 600 700 800 900 1000 ;
-python prepareCards.py -y run2 -c C  --For specific --coupling_value rtc0p4 --Masses 200 300 350 400 500 600 700 800 900 1000 ;
-
+#rtu0p1
+#2016apv
+python prepareCards.py -y 2016apv -c em -reg 'SR_em' --For specific --coupling_value rtu0p1 --Masses 200 300 350 400 500 600 700 800 900 1000 --scale;
+python prepareCards.py -y 2016apv -c ee -reg 'SR_ee' --For specific --coupling_value rtu0p1 --Masses 200 300 350 400 500 600 700 800 900 1000 --scale;
+python prepareCards.py -y 2016apv -c mm -reg 'SR_mm' --For specific --coupling_value rtu0p1 --Masses 200 300 350 400 500 600 700 800 900 1000 --scale;
+python prepareCards.py -y 2016apv -c C  --For specific --coupling_value rtu0p1 --Masses 200 300 350 400 500 600 700 800 900 1000 --scale;
+#2016postapv
+python prepareCards.py -y 2016postapv -c em -reg 'SR_em' --For specific --coupling_value rtu0p1 --Masses 200 300 350 400 500 600 700 800 900 1000 --scale;
+python prepareCards.py -y 2016postapv -c ee -reg 'SR_ee' --For specific --coupling_value rtu0p1 --Masses 200 300 350 400 500 600 700 800 900 1000 --scale;
+python prepareCards.py -y 2016postapv -c mm -reg 'SR_mm' --For specific --coupling_value rtu0p1 --Masses 200 300 350 400 500 600 700 800 900 1000 --scale;
+python prepareCards.py -y 2016postapv -c C  --For specific --coupling_value rtu0p1 --Masses 200 300 350 400 500 600 700 800 900 1000 --scale;
+#2017
+python prepareCards.py -y 2017 -c em -reg 'SR_em' --For specific --coupling_value rtu0p1 --Masses 200 300 350 400 500 600 700 800 900 1000 --scale;
+python prepareCards.py -y 2017 -c ee -reg 'SR_ee' --For specific --coupling_value rtu0p1 --Masses 200 300 350 400 500 600 700 800 900 1000 --scale;
+python prepareCards.py -y 2017 -c mm -reg 'SR_mm' --For specific --coupling_value rtu0p1 --Masses 200 300 350 400 500 600 700 800 900 1000 --scale;
+python prepareCards.py -y 2017 -c C  --For specific --coupling_value rtu0p1 --Masses 200 300 350 400 500 600 700 800 900 1000 --scale;
+#2018
+python prepareCards.py -y 2018 -c em -reg 'SR_em' --For specific --coupling_value rtu0p1 --Masses 200 300 350 400 500 600 700 800 900 1000 --scale;
+python prepareCards.py -y 2018 -c ee -reg 'SR_ee' --For specific --coupling_value rtu0p1 --Masses 200 300 350 400 500 600 700 800 900 1000 --scale;
+python prepareCards.py -y 2018 -c mm -reg 'SR_mm' --For specific --coupling_value rtu0p1 --Masses 200 300 350 400 500 600 700 800 900 1000 --scale;
+python prepareCards.py -y 2018 -c C  --For specific --coupling_value rtu0p1 --Masses 200 300 350 400 500 600 700 800 900 1000 --scale; 
+#run2
+python prepareCards.py -y run2 -c em -reg 'SR_em' --For specific --coupling_value rtu0p1 --Masses 200 300 350 400 500 600 700 800 900 1000 --scale;
+python prepareCards.py -y run2 -c ee -reg 'SR_ee' --For specific --coupling_value rtu0p1 --Masses 200 300 350 400 500 600 700 800 900 1000 --scale;
+python prepareCards.py -y run2 -c mm -reg 'SR_mm' --For specific --coupling_value rtu0p1 --Masses 200 300 350 400 500 600 700 800 900 1000 --scale;
+python prepareCards.py -y run2 -c C  --For specific --coupling_value rtu0p1 --Masses 200 300 350 400 500 600 700 800 900 1000 --scale;
+#rtu0p8
+#2016apv
+python prepareCards.py -y 2016apv -c em -reg 'SR_em' --For specific --coupling_value rtu0p8 --Masses 200 300 350 400 500 600 700 800 900 1000 --scale;
+python prepareCards.py -y 2016apv -c ee -reg 'SR_ee' --For specific --coupling_value rtu0p8 --Masses 200 300 350 400 500 600 700 800 900 1000 --scale;
+python prepareCards.py -y 2016apv -c mm -reg 'SR_mm' --For specific --coupling_value rtu0p8 --Masses 200 300 350 400 500 600 700 800 900 1000 --scale;
+python prepareCards.py -y 2016apv -c C  --For specific --coupling_value rtu0p8 --Masses 200 300 350 400 500 600 700 800 900 1000 --scale;
+#2016postapv
+python prepareCards.py -y 2016postapv -c em -reg 'SR_em' --For specific --coupling_value rtu0p8 --Masses 200 300 350 400 500 600 700 800 900 1000 --scale;
+python prepareCards.py -y 2016postapv -c ee -reg 'SR_ee' --For specific --coupling_value rtu0p8 --Masses 200 300 350 400 500 600 700 800 900 1000 --scale;
+python prepareCards.py -y 2016postapv -c mm -reg 'SR_mm' --For specific --coupling_value rtu0p8 --Masses 200 300 350 400 500 600 700 800 900 1000 --scale;
+python prepareCards.py -y 2016postapv -c C  --For specific --coupling_value rtu0p8 --Masses 200 300 350 400 500 600 700 800 900 1000 --scale;
+#2017
+python prepareCards.py -y 2017 -c em -reg 'SR_em' --For specific --coupling_value rtu0p8 --Masses 200 300 350 400 500 600 700 800 900 1000 --scale;
+python prepareCards.py -y 2017 -c ee -reg 'SR_ee' --For specific --coupling_value rtu0p8 --Masses 200 300 350 400 500 600 700 800 900 1000 --scale;
+python prepareCards.py -y 2017 -c mm -reg 'SR_mm' --For specific --coupling_value rtu0p8 --Masses 200 300 350 400 500 600 700 800 900 1000 --scale;
+python prepareCards.py -y 2017 -c C  --For specific --coupling_value rtu0p8 --Masses 200 300 350 400 500 600 700 800 900 1000 --scale;
+#2018
+python prepareCards.py -y 2018 -c em -reg 'SR_em' --For specific --coupling_value rtu0p8 --Masses 200 300 350 400 500 600 700 800 900 1000 --scale;
+python prepareCards.py -y 2018 -c ee -reg 'SR_ee' --For specific --coupling_value rtu0p8 --Masses 200 300 350 400 500 600 700 800 900 1000 --scale;
+python prepareCards.py -y 2018 -c mm -reg 'SR_mm' --For specific --coupling_value rtu0p8 --Masses 200 300 350 400 500 600 700 800 900 1000 --scale;
+python prepareCards.py -y 2018 -c C  --For specific --coupling_value rtu0p8 --Masses 200 300 350 400 500 600 700 800 900 1000 --scale; 
+#run2
+python prepareCards.py -y run2 -c em -reg 'SR_em' --For specific --coupling_value rtu0p8 --Masses 200 300 350 400 500 600 700 800 900 1000 --scale;
+python prepareCards.py -y run2 -c ee -reg 'SR_ee' --For specific --coupling_value rtu0p8 --Masses 200 300 350 400 500 600 700 800 900 1000 --scale;
+python prepareCards.py -y run2 -c mm -reg 'SR_mm' --For specific --coupling_value rtu0p8 --Masses 200 300 350 400 500 600 700 800 900 1000 --scale;
+python prepareCards.py -y run2 -c C  --For specific --coupling_value rtu0p8 --Masses 200 300 350 400 500 600 700 800 900 1000 --scale;
+#rtu1p0
+#2016apv
+python prepareCards.py -y 2016apv -c em -reg 'SR_em' --For specific --coupling_value rtu1p0 --Masses 200 300 350 400 500 600 700 800 900 1000 --scale;
+python prepareCards.py -y 2016apv -c ee -reg 'SR_ee' --For specific --coupling_value rtu1p0 --Masses 200 300 350 400 500 600 700 800 900 1000 --scale;
+python prepareCards.py -y 2016apv -c mm -reg 'SR_mm' --For specific --coupling_value rtu1p0 --Masses 200 300 350 400 500 600 700 800 900 1000 --scale;
+python prepareCards.py -y 2016apv -c C  --For specific --coupling_value rtu1p0 --Masses 200 300 350 400 500 600 700 800 900 1000 --scale;
+#2016postapv
+python prepareCards.py -y 2016postapv -c em -reg 'SR_em' --For specific --coupling_value rtu1p0 --Masses 200 300 350 400 500 600 700 800 900 1000 --scale;
+python prepareCards.py -y 2016postapv -c ee -reg 'SR_ee' --For specific --coupling_value rtu1p0 --Masses 200 300 350 400 500 600 700 800 900 1000 --scale;
+python prepareCards.py -y 2016postapv -c mm -reg 'SR_mm' --For specific --coupling_value rtu1p0 --Masses 200 300 350 400 500 600 700 800 900 1000 --scale;
+python prepareCards.py -y 2016postapv -c C  --For specific --coupling_value rtu1p0 --Masses 200 300 350 400 500 600 700 800 900 1000 --scale;
+#2017
+python prepareCards.py -y 2017 -c em -reg 'SR_em' --For specific --coupling_value rtu1p0 --Masses 200 300 350 400 500 600 700 800 900 1000 --scale;
+python prepareCards.py -y 2017 -c ee -reg 'SR_ee' --For specific --coupling_value rtu1p0 --Masses 200 300 350 400 500 600 700 800 900 1000 --scale;
+python prepareCards.py -y 2017 -c mm -reg 'SR_mm' --For specific --coupling_value rtu1p0 --Masses 200 300 350 400 500 600 700 800 900 1000 --scale;
+python prepareCards.py -y 2017 -c C  --For specific --coupling_value rtu1p0 --Masses 200 300 350 400 500 600 700 800 900 1000 --scale;
+#2018
+python prepareCards.py -y 2018 -c em -reg 'SR_em' --For specific --coupling_value rtu1p0 --Masses 200 300 350 400 500 600 700 800 900 1000 --scale;
+python prepareCards.py -y 2018 -c ee -reg 'SR_ee' --For specific --coupling_value rtu1p0 --Masses 200 300 350 400 500 600 700 800 900 1000 --scale;
+python prepareCards.py -y 2018 -c mm -reg 'SR_mm' --For specific --coupling_value rtu1p0 --Masses 200 300 350 400 500 600 700 800 900 1000 --scale;
+python prepareCards.py -y 2018 -c C  --For specific --coupling_value rtu1p0 --Masses 200 300 350 400 500 600 700 800 900 1000 --scale; 
+#run2
+python prepareCards.py -y run2 -c em -reg 'SR_em' --For specific --coupling_value rtu1p0 --Masses 200 300 350 400 500 600 700 800 900 1000 --scale;
+python prepareCards.py -y run2 -c ee -reg 'SR_ee' --For specific --coupling_value rtu1p0 --Masses 200 300 350 400 500 600 700 800 900 1000 --scale;
+python prepareCards.py -y run2 -c mm -reg 'SR_mm' --For specific --coupling_value rtu1p0 --Masses 200 300 350 400 500 600 700 800 900 1000 --scale;
+python prepareCards.py -y run2 -c C  --For specific --coupling_value rtu1p0 --Masses 200 300 350 400 500 600 700 800 900 1000 --scale;
