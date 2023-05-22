6f4297d500e1dc7a84c70308eb73dc760b27c58c
AlbertHsuNTUphys <b07202037@ntu.edu.tw>
2023-05-22 16:39:55 +0200

    [Toolkit] Update toolkit package

---
+++ example/step1_InitAndRebin.sh
@@ -0,0 +1,20 @@
+python Init.py --year 2017 --channel all
+python Init.py --year 2018 --channel all
+python Init.py --year 2016apv --channel all
+python Init.py --year 2016postapv --channel all
+
+unblind="${2:-''}"
+
+for Era in 2016apv 2016postapv 2017 2018
+do 
+  for coupling in 0p4
+  do
+    for coupling_name in rtc rtu
+    do
+      python ReBin.py -c all --Couplings $coupling --Coupling_Name $coupling_name --y $Era --Masses 200 300 350 400 500 600 700 800 900 1000 --inputdir $1 $unblind;
+      python ReBin.py -c all --Couplings $coupling --Coupling_Name $coupling_name --y $Era --Masses 250 300 350 400 550 700 800 900 1000 --inputdir $1 --interference $unblind;
+    done
+  done
+done
+
+ 
---
+++ example/step6_1_batch_plot_impact.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+
+###########################################################################
+# :Usage                                                                  #  
+#  - Use tmux to run plot_Impact.sh in batch                              #
+# :Command                                                                #
+#  - sh quick_tmux_batch.sh  ./                                           #
+# :Input parameter(order is important)                                    #
+#  - 1: rtu/rtc                                                           #
+#  - 2: output directory                                                  #
+###########################################################################
+
+RED='\033[0;31m'
+NC='\033[0m'
+UBlue='\033[4;34m'        # Blue
+BBlack='\033[1;30m'       # Black
+BRed='\033[1;31m'         # Red
+BGreen='\033[1;32m'       # Green
+BYellow='\033[1;33m'      # Yellow
+BBlue='\033[1;34m'        # Blue
+BPurple='\033[1;35m'      # Purple
+BCyan='\033[1;36m'        # Cyan
+BWhite='\033[1;37m'       # White
+
+outdir=${2:-"./"}
+workdir=$(pwd)
+unblind=${3:-""}
+
+for ERA in run2
+do
+  for channel in C
+  do
+    for fit_model in b_only
+    do
+      for interfere in interference pure
+      do
+        for mass in 350 900
+        do
+          # Remember for 2016-2018, default minimizer setting is 1 0.1
+          #          for run2,                                   0 1.0
+          if [ $ERA == run2 ]; then
+            echo -e "${BCyan}[tmux: ${ERA}_${channel}_${fit_model}_${interfere}_${mass}]${NC} ${BYellow}sh example/plot_Impact.sh ${ERA} ${channel} ${mass} ${1}04 ${fit_model} ${interfere} 0 1.0 ${outdir} ${unblind}${NC}"
+            tmux new-session -d -s $ERA\_$channel\_$fit_model\_$interfere\_$mass "sh example/plot_Impact.sh ${ERA} ${channel} ${mass} ${1}04 ${fit_model} ${interfere} 0 1.0 ${outdir} ${unblind}"
+          else
+            echo -e "${BCyan}[tmux: ${ERA}_${channel}_${fit_model}_${interfere}_${mass}]${NC} ${BYellow}sh example/plot_Impact.sh ${ERA} ${channel} ${mass} ${1}04 ${fit_model} ${interfere} 1 0.1 ${outdir} ${unblind}${NC}"
+            tmux new-session -d -s $ERA\_$channel\_$fit_model\_$interfere\_$mass "sh example/plot_Impact.sh ${ERA} ${channel} ${mass} ${1}04 ${fit_model} ${interfere} 1 0.1 ${outdir} ${unblind}"
+          fi
+        done
+      done
+    done
+  done
+done
