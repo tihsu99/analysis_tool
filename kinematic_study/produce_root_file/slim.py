6c7d4272f933de15e3ea1e7f33084a89a709e751
AlbertHsuNTUphys <b07202037@ntu.edu.tw>
2024-02-20 09:10:46 +0100

    [produce root file] add BDT

--- kinematic_study/produce_root_file/slim.py
+++ kinematic_study/produce_root_file/slim.py
@@ -6,8 +6,10 @@ import json
 import optparse, argparse
 from collections import OrderedDict
 from math import sqrt
-from common import inputFile_path
+from common import inputFile_path, read_json
+import ROOT
 
+cwd = os.getcwd()
 
 def Slim_module(filein,
                 era,
@@ -17,7 +19,7 @@ def Slim_module(filein,
                 Black_list=[],
                 POIs=[],
                 sample_labels = [],
-                weight_def="puWeight*genWeight*L1PreFiringWeight_Nom/abs(genWeight)*Trigger_sf*Lepton_ID_SF*Lepton_RECO_SF",
+                weight_def="puWeight*genWeight*L1PreFiringWeight_Nom/abs(genWeight)*Lepton_ID_SF*Lepton_RECO_SF*btag_DeepJet_SF*Trigger_sf",
                 scale = 1.0,
                 sample_json = "../../data/sample.json",
                 nuisance_json = "../../data/nuisance.json",
@@ -26,6 +28,8 @@ def Slim_module(filein,
                 trigger_json = "../../data/trigger.json",
                 MET_filter_json = "../../data/MET_filter.json",
                 histogram_json = "../../data/histogram.json",
+                MVA_json = "../../data/MVA.json",
+                MVA_weight_dir = "../MVA_study/MVA_Training_Weight/",
                 region="signal_region",
                 Btag_WP="Medium",
                 start = -1,
@@ -51,11 +55,16 @@ def Slim_module(filein,
 
   path    = str(inputFile_path[era])
   fin     = os.path.join(path, filein)
+  if 'eos' in fin and 'root://eosuser.cern.ch//' not in fin:
+      fin = 'root://eosuser.cern.ch//' + fin
+
   if not index == -1:
     fileOut = os.path.join(output_dir, str(index) + "_" + filein)
+    fileOut_alt = os.path.join(cwd, str(index) + "_" + filein)
   else:
     fileOut = os.path.join(output_dir, filein) 
-  treeOut = "SlimTree"
+    fileOut_alt = os.path.join(cwd, str(index) + "_" + filein)
+  treeOut = "Events"
 
   if not os.path.isdir(output_dir):
     os.system("mkdir -p " + output_dir)
@@ -139,6 +148,10 @@ def Slim_module(filein,
         df = df.Define(str(variable), str(variables[variable]["Category"][Btag_WP]))
       else:
         df = df.Define(str(variable), str(variables[variable]["Def"]))
+      if("Children" in variables[variable]):
+          for child_no, child_name in enumerate(variables[variable]["Children"]):
+              df = df.Define(str(child_name), str("{}[{}]".format(variable, child_no)))
+
     if variable in nuisances_valid:
       for nuisance in nuisances_valid[variable]:
         if "Era" in nuisances[nuisance] and era not in nuisances[nuisance]["Era"]: continue
@@ -148,7 +161,7 @@ def Slim_module(filein,
         if "sub_cat" in nuisances[nuisance]: sub_category = nuisances[nuisance]["sub_cat"]
         for sub_cat in sub_category:
           nuisance_def =  nuisances[nuisance]["Def"].replace('SUB', sub_cat).replace('YEAR', era)
-          nuisance_name = nuisance+sub_cat.replace('YEAR', era).replace('CHANNEL', channel)
+          nuisance_name = (nuisance+sub_cat).replace('YEAR', era).replace('CHANNEL', channel)
           print(nuisances[nuisance]["Nominal"], nuisance_def, nuisance_name)
           df = df.Vary(nuisances[nuisance]["Nominal"], nuisance_def, {"Down", "Up"}, nuisance_name)
           nuisance_list.append(nuisance_name)
@@ -183,17 +196,24 @@ def Slim_module(filein,
     df = df.Filter(str(cuts[region]["channel_cut"][channel][cut_name]), str(cut_name))
   cutflow["channel"] = df.Sum("weight").GetValue()
   # trigger cut
+  trigger_cut = None
   for trigger_name in triggers:
     if not channel in triggers[trigger_name]["Channel"]: continue
     if "Data" in sample_labels and not sample_name in triggers[trigger_name]["Dataset"]: continue
-    trigger_set = []
-    for trigger_ in triggers[trigger_name]['Triggers'][era]:
-        if trigger_ in BranchList:
-            trigger_set.append(trigger_)
-    df = df.Define(str(trigger_name), str('||'.join(trigger_set)))
+    if not "Data" in sample_labels: 
+      trigger_cut = str(triggers[trigger_name]["Triggers"][era]["MC"])
+    else:
+      sub_era = filein.replace('.root', '').replace(sample_name + "_", '') 
+      if sub_era in triggers[trigger_name]["Triggers"][era][sample_name]:
+        trigger_cut = triggers[trigger_name]["Triggers"][era][sample_name][sub_era]
+      else:
+        trigger_cut = triggers[trigger_name]["Triggers"][era][sample_name]["Default"]
+
+
+    df = df.Define(str(trigger_name), str(trigger_cut))
     df = df.Filter(str(trigger_name), str(trigger_name))
-    print(trigger_name, str('||'.join(trigger_set)))
-  cutflow[trigger_name] = df.Sum("weight").GetValue() 
+    print(trigger_name, str(trigger_cut))
+    cutflow[trigger_name] = df.Sum("weight").GetValue() 
   # general cut
   for cut_name in cuts[region]["general_cut"]:
     df = df.Filter(str(cuts[region]["general_cut"][cut_name]), str(cut_name))
@@ -211,6 +231,22 @@ def Slim_module(filein,
 
   print(cutflow)
 
+  ####################
+  ##  MVA Variable  ##
+  ####################
+
+  MVA_json_dict = read_json(MVA_json)
+  BDT = dict()
+  if MVA_weight_dir is not None:
+    MVA_list = os.listdir(MVA_weight_dir)
+    for MVA in MVA_list:
+        ROOT.gInterpreter.ProcessLine('''
+        TMVA::Experimental::RBDT<> %s ("XGB", "%s");
+        computeModel_%s = TMVA::Experimental::Compute<%d, float> (%s);
+        '''%(MVA, os.path.join(MVA_weight_dir, MVA, "XGB.root"), MVA, len(MVA_json_dict["xgboost"]), MVA))
+        df = df.Define(str(MVA), eval("ROOT.computeModel_%s"%MVA), MVA_json_dict["xgboost"])
+        POIs.append(MVA)
+
   #################
   ##  Histogram  ##
   #################
@@ -219,6 +255,17 @@ def Slim_module(filein,
   jsonfile   = open(histogram_json)
   Histograms = json.load(jsonfile, object_pairs_hook=OrderedDict)
   jsonfile.close()
+
+
+  if MVA_weight_dir is not None:
+    for MVA in MVA_list:
+      Histograms[MVA] = {
+        "Title": ";BDT;nEntries",
+        "xlow":0,
+        "xhigh":1,
+        "nbin": 10,
+        "Label": ["Reco","Normal", "BDT"]
+      }
 
   for Histogram in Histograms:
     print("Generating", Histogram)
@@ -274,9 +321,18 @@ def Slim_module(filein,
       if "Save" in variables[variable] and Label in variables[variable]["Save"]: Flag = True
     if not Flag: continue
     columns.push_back(str(variable))
-
+    if "Children" in variables[variable]:
+        for child_ in variables[variable]["Children"]:
+            columns.push_back(str(child_))
+   
+  if not "Data" in sample_labels:
+    columns.push_back('weight')
+    df = df.Define("weight_n_Norm", "weight * %f"%(scale))
+    columns.push_back('weight_n_Norm')
+  
+  if 'eos' in fileOut and 'root://eosuser.cern.ch//' not in fileOut:
+    fileOut = 'root://eosuser.cern.ch//{}'.format(fileOut)
   df.Snapshot(treeOut, fileOut, columns)
-
   #######################
   ##  Store Histogram  ##
   #######################
@@ -289,6 +345,7 @@ def Slim_module(filein,
       h.Scale(scale) # Lumi x xSec / nDAS (input from runCondor)
     h.Write()
   FileOut.Close()
+
 
 
 if __name__ == "__main__":
@@ -315,10 +372,15 @@ if __name__ == "__main__":
   parser.add_argument("--sample_labels", dest='sample_labels', default = ["MC", "Background"], nargs='+')
   parser.add_argument("--Btag_WP", default='Medium')
   parser.add_argument("--POIs",   dest = 'POIs',   default = [], nargs='+')
-  parser.add_argument("--scale",  dest = 'scale',  default = 1.0, type=float) 
+  parser.add_argument("--scale",  dest = 'scale',  default = 1.0, type=float)
+  parser.add_argument("--MVA_json", default = "../../data/MVA.json", type=str)
+  parser.add_argument("--MVA_weight_dir", default = None, type=str)
   args = parser.parse_args()
   if "DEFAULT" in args.POIs: args.POIs = []
+  if args.MVA_weight_dir == "None": args.MVA_weight_dir = None
   #start = time.clock()
+
+
   Slim_module(args.iin, args.era, args.out, start = args.start, end = args.end, index = args.index, channel = args.channel, \
               sample_json = args.sample_json,\
               cut_json = args.cut_json,\
@@ -328,6 +390,8 @@ if __name__ == "__main__":
               MET_filter_json = args.MET_filter_json,\
               nuisance_json = args.nuisance_json,\
               Btag_WP = args.Btag_WP,\
+              MVA_json = args.MVA_json,\
+              MVA_weight_dir = args.MVA_weight_dir, \
               region = args.region, Labels = args.Labels,Black_list = args.Black_list, POIs = args.POIs, sample_labels = args.sample_labels, scale = args.scale)
   #end = time.clock()
   #print('process time', end - start)
