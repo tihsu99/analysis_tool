44a6c1dae1fa2819e8757a4c3818a824b2348e4d
AlbertHsuNTUphys <b07202037@ntu.edu.tw>
2024-02-20 09:09:37 +0100

    [signal_study] init

---
+++ kinematic_study/signal_study/Farm/slim_CGToBHpm_a_600_rtt06_rtc04.root_elepT35_mupT27.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+WORKDIR=/afs/cern.ch/user/t/tihsu/bHplus/CMSSW_11_3_4/src/analysis_tool/kinematic_study/signal_study
+cd /afs/cern.ch/user/t/tihsu/bHplus/CMSSW_11_3_4
+cd ${WORKDIR}
+source script/env.sh
+python /afs/cern.ch/user/t/tihsu/bHplus/CMSSW_11_3_4/src/analysis_tool/kinematic_study/signal_study/calculate_signal_efficiency.py --dataset CGToBHpm_a_600_rtt06_rtc04 --iin CGToBHpm_a_600_rtt06_rtc04.root --era 2017 --ele_pt 35 --mu_pt 27 --normfactor 0.025844852289819916 --outdir ./ele_35_mu_27
---
+++ kinematic_study/signal_study/Farm/slim_CGToBHpm_a_700_rtt06_rtc04.root_elepT32_mupT27.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+WORKDIR=/afs/cern.ch/user/t/tihsu/bHplus/CMSSW_11_3_4/src/analysis_tool/kinematic_study/signal_study
+cd /afs/cern.ch/user/t/tihsu/bHplus/CMSSW_11_3_4
+cd ${WORKDIR}
+source script/env.sh
+python /afs/cern.ch/user/t/tihsu/bHplus/CMSSW_11_3_4/src/analysis_tool/kinematic_study/signal_study/calculate_signal_efficiency.py --dataset CGToBHpm_a_700_rtt06_rtc04 --iin CGToBHpm_a_700_rtt06_rtc04.root --era 2017 --ele_pt 32 --mu_pt 27 --normfactor 0.012080990811617367 --outdir ./ele_32_mu_27
