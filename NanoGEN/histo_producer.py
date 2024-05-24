bcb5aefa84b6c30844695517ce0de35258ec8d7e
Agostino De Iorio <agostino.de.iorio@cern.ch>
2024-05-24 10:43:03 +0200

    macro to run new_bH_GenWeight for all NanoGEN

--- NanoGEN/histo_producer.py
+++ NanoGEN/histo_producer.py
@@ -4,20 +4,17 @@ path = "/eos/cms/store/group/phys_b2g/ExYukawa/bHplus/NanoGEN_2017/v3/"
 
 productions = ["bgth", "cgbh"]
 productions = ["cgbh"]
-masses = [200 ]#, 300, 350, 400, 500, 600, 700, 800, 900, 1000] #[mass for mass in range(200, 1100, 100)]
+masses = [200, 300, 350, 400, 500, 600, 700, 800, 900, 1000]
 print(masses)
-rhotts = ["01" ]#, "04", "06", "10"]
-rhotcs = ["01" ]#, "04", "06", "10"]
-
-# bgth_H_M800_rhott01_rhotc01_rhotu00.root
+rhotts = ["01", "04", "06", "10"]
+rhotcs = ["01", "04", "06", "10"]
 
 for prod in productions:
     for mass in masses:
         for rhott in rhotts:
             for rhotc in rhotcs:
-                #filename = "bgth_H_M800_rhott01_rhotc01_rhotu00"
                 filename = f"{prod}_H_M{mass}_rhott{rhott}_rhotc{rhotc}_rhotu00"
                 print(f"Producing histograms for {filename}")
-                os.system(f"python3 bH_GenWeight.py -i {path}{filename}.root -o {filename}_hist")
+                os.system(f"python3 new_bH_GenWeight.py -i {path}{filename}.root -o {filename}_hist")
 
                 
