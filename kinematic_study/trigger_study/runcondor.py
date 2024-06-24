5f762e5f719e49c4282bb4f292cf84d3b9091fe0
AlbertHsuNTUphys <b07202037@ntu.edu.tw>
2024-06-24 10:36:59 +0200

    [Trigger study] update v2 usage

--- kinematic_study/trigger_study/runcondor.py
+++ kinematic_study/trigger_study/runcondor.py
@@ -8,18 +8,60 @@ from collections import OrderedDict
 sys.path.insert(1, '../../python')
 from common import *
 import re
+from termcolor import colored, cprint
 
 fileset = dict()
-fileset['2017'] = {
-        'Data': ['MET_{}.root'.format(subera) for subera in subera_list['2017']],
-        'TT1L': ['TTTo1L_{}.root'.format(subfile_idx) for subfile_idx in range(1,11)],
-        'TT2L': ['TTTo2L_{}.root'.format(subfile_idx) for subfile_idx in range(1,4)],
-}
-fileset['2018'] = {  
-        'Data': [ 'MET_{}.root'.format(subera) for subera in subera_list['2018']],
-        'TT1L': ['TTTo1L_{}.root'.format(subfile_idx) for subfile_idx in range(1,14)],
-        'TT2L': ['TTTo2L_{}.root'.format(subfile_idx) for subfile_idx in range(1,6)]
 
+fileset['2016apv'] = {
+  'Electron': {
+    'Data': ['SingleMuon_{}.root'.format(subera) for subera in subera_list['2016apv']],
+    'TT1L': ['TTTo1L_{}.root'.format(subfile_idx) for subfile_idx in range(1,5)],
+    'TT2L': ['TTTo2L.root']
+  },
+  'Muon': {
+    'Data': ['SingleEG_{}.root'.format(subera) for subera in subera_list['2016apv']] +  ['SinglePhoton_{}.root'.format(subera) for subera in subera_list['2016apv']],
+    'TT1L': ['TTTo1L_{}.root'.format(subfile_idx) for subfile_idx in range(1,5)],
+    'TT2L': ['TTTo2L.root']
+  }
+}
+
+fileset['2016postapv'] = {
+  'Electron': {
+    'Data': ['SingleMuon_{}.root'.format(subera) for subera in subera_list['2016postapv']],
+    'TT1L': ['TTTo1L_{}.root'.format(subfile_idx) for subfile_idx in range(1,6)],
+    'TT2L': ['TTTo2L.root']
+  },
+  'Muon': {
+    'Data': ['SingleEG_{}.root'.format(subera) for subera in subera_list['2016postapv']] + ['SinglePhoton_{}.root'.format(subera) for subera in subera_list['2016postapv']],
+    'TT1L': ['TTTo1L_{}.root'.format(subfile_idx) for subfile_idx in range(1,6)],
+    'TT2L': ['TTTo2L.root']
+  }
+}
+
+fileset['2017'] = {
+  'Electron': {
+    'Data': ['SingleMuon_{}.root'.format(subera) for subera in subera_list['2017']],
+    'TT1L': ['TTTo1L_{}.root'.format(subfile_idx) for subfile_idx in range(1,11)],
+    'TT2L': ['TTTo2L_{}.root'.format(subfile_idx) for subfile_idx in range(1,4)],
+  },
+  'Muon': {
+    'Data': ['SingleEG_{}.root'.format(subera) for subera in subera_list['2017']] + ['SinglePhoton_{}.root'.format(subera) for subera in subera_list['2017']],
+    'TT1L': ['TTTo1L_{}.root'.format(subfile_idx) for subfile_idx in range(1,11)],
+    'TT2L': ['TTTo2L_{}.root'.format(subfile_idx) for subfile_idx in range(1,4)],
+  }
+}
+
+fileset['2018'] = { 
+  'Electron':{
+    'Data': ['SingleMuon_{}.root'.format(subera) for subera in subera_list['2018']],
+    'TT1L': ['TTTo1L_{}.root'.format(subfile_idx) for subfile_idx in range(1,14)],
+    'TT2L': ['TTTo2L_{}.root'.format(subfile_idx) for subfile_idx in range(1,6)]
+  },
+  'Muon':{
+    'Data': ['EGamma_{}.root'.format(subera) for subera in subera_list['2018']],
+    'TT1L': ['TTTo1L_{}.root'.format(subfile_idx) for subfile_idx in range(1,14)],
+    'TT2L': ['TTTo2L_{}.root'.format(subfile_idx) for subfile_idx in range(1,6)]
+  }
 }
 
 
@@ -31,7 +73,7 @@ if __name__ == "__main__":
   parser.add_argument('-e', '--era',    dest='era',    help='[all/2016apv/2016postapv/2017/2018]',default='all',type=str, choices=["all","2016apv","2016postapv","2017","2018"])
   parser.add_argument('--region',       dest='region', default='all', type=str, choices=['all', 'bh', 'boost'])
   parser.add_argument('--lepton',       dest='lepton', default='all', type=str, choices=['all', 'Electron', 'Muon'])
-  parser.add_argument('--JobFlavour', dest = 'JobFlavour', help='espresso/microcentury/longlunch/workday/tomorrow', type=str, default='longlunch')
+  parser.add_argument('--JobFlavour', dest = 'JobFlavour', help='espresso/microcentury/longlunch/workday/tomorrow', type=str, default='workday')
   parser.add_argument('--universe',   dest = 'universe', help='vanilla/local', type=str, default='vanilla')
   parser.add_argument('--outdir',     dest = 'outdir',     help='output directory',   type=str, default='./')
   parser.add_argument("--test",       action = "store_true")
@@ -45,7 +87,6 @@ if __name__ == "__main__":
   ##  Path  ##
   ############
 
-  #cmsswBase = os.environ['CMSSW_BASE']
   farm_dir  = os.path.join('./', 'Farm')
   cwd       = os.getcwd()
 
@@ -104,8 +145,8 @@ if __name__ == "__main__":
   condor.write('log    = %s/job_common_$(Process).log\n'%farm_dir)
   condor.write('executable = %s/$(cfgFile)\n'%farm_dir)
   condor.write('universe = %s\n'%args.universe)
-  condor.write('requirements = (OpSysAndVer =?= "CentOS7")\n')
   condor.write('+JobFlavour = "%s"\n'%args.JobFlavour)
+  condor.write('queue 1 cfgFile in ')
   #condor.write('+MaxRuntime = 7200\n')
   cwd = os.getcwd()
 
@@ -123,7 +164,7 @@ if __name__ == "__main__":
        os.system('mkdir -p script')
        os.system('cp %s/../../script/env.sh script/.'%cwd)
 
-       file_dict = fileset[Era] 
+       file_dict = fileset[Era][lepton]
        for dataset in file_dict:
          for iin in file_dict[dataset]:
            python_file   =  os.path.join(cwd, 'skim.py')
@@ -137,18 +178,27 @@ if __name__ == "__main__":
              nDAS  = 0
              for file_ in file_dict[dataset]:
                if ((sample_name + "_") in file_) or ((sample_name + ".") in file_):
-                 ftemp = ROOT.TFile.Open(os.path.join(inputFile_path[Era],file_).replace('v2','v3'), "READ")
+                 ftemp = ROOT.TFile.Open(os.path.join(inputFile_path[Era],file_), "READ")
                  nDAS += ftemp.Get('nEventsGenWeighted').GetBinContent(1)
                  ftemp.Close()
              norm_factor = Lumi[Era]*samples[sample_name]['xsec']/float(nDAS)
-
            if(args.check):
-                fname = os.path.join(Outdir, variation, "{}_{}_".format(region, lepton) + iin)
+                fname = os.path.join(Outdir, Era, variation, "{}_{}_{}".format(region, lepton, iin))
+                print('checking {}......'.format(fname))
                 try:
-                   h = ROOT.TFile.Open(fname,'READ').Get("num")
+                   f = ROOT.TFile.Open(fname,'READ')
+                   h1 = f.Get('num')
+                   h1.SetDirectory(0)
+                   h2 = f.Get('den')
+                   h2.SetDirectory(0)
+                   f.Close()
+                   h1 = h1.Clone()
+                   h2 = h2.Clone()
+                   cprint('success', 'green')
                    continue
                 except Exception as e:
-                   print(e)
+                   #cprint(e, 'red')
+                   cprint('reproduce {}'.format(fname), 'yellow')
                    Check_GreenLight=False
            shell_file = "slim_%s_%s_%s_%s_%s.sh"%(iin, Era, region, lepton, variation)
            command = 'python skim.py --era %s --iin %s --dataset %s --outdir %s --region %s --lepton %s --normfactor %f --variation %s'%(Era, iin, dataset, Outdir, region, lepton, norm_factor, variation)
@@ -156,11 +206,11 @@ if __name__ == "__main__":
              if dataset == 'Data': prepare_shell(shell_file, command, condor, farm_dir)
            else:
              prepare_shell(shell_file, command, condor, farm_dir)
-
+           print(Era, region, lepton, variation)
   ##################
   ##  Submit Job  ##
   ##################
-
+  cprint(Check_GreenLight, 'yellow')
   condor.close()
   if not args.test and not (args.check and Check_GreenLight):
     print("Submitting Jobs on Condor")
