2c41aa721ac7da955aac113e9c74243b0c71d970
AlbertHsuNTUphys <b07202037@ntu.edu.tw>
2024-09-04 08:29:19 +0200

    [python] update common

--- python/common.py
+++ python/common.py
@@ -29,7 +29,7 @@ python_version = int(sys.version.split('.')[0])
 inputFile_path = {
    '2016apv':     '/eos/cms/store/group/phys_b2g/ExYukawa/bHplus/2016apv/v6/', 
    '2016postapv': '/eos/cms/store/group/phys_b2g/ExYukawa/bHplus/2016/v6/', 
-   '2017':        '/eos/cms/store/group/phys_b2g/ExYukawa/bHplus/2017/v6/', 
+   '2017':        '/eos/cms/store/group/phys_b2g/ExYukawa/bHplus/2017/v7/', 
    '2018':        '/eos/cms/store/group/phys_b2g/ExYukawa/bHplus/2018/v6/'
 }
 
