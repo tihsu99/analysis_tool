5bfdb172088857bc975505295c060a02ddd899c9
Gouranga Kole <gouranga.tifr@gmail.com>
2024-10-07 14:50:25 +0200

    back to terry but with a comment :)

--- kinematic_study/produce_root_file/runcondor.py
+++ kinematic_study/produce_root_file/runcondor.py
@@ -562,7 +562,7 @@ if __name__ == "__main__":
               os.system('chmod +x {}/{}.sh'.format(farm_dir, 'merge_{}_{}_{}_{}'.format(Era, region, channel, process)))
               merge_shell[Era][region][channel][process] = open(os.path.join(farm_dir, 'merge_{}_{}_{}_{}.sh'.format(Era, region, channel, process)), 'a')
               merge_shell[Era][region][channel][process].write('\n')
-              # merge_shell[Era][region][channel][process].write('rm {}/*slim_*_{}_{}_{}_*_{}*.out\n'.format(farm_dir, Era, region, channel, process))
+              merge_shell[Era][region][channel][process].write('rm {}/*slim_*_{}_{}_{}_*_{}*.out\n'.format(farm_dir, Era, region, channel, process)) # triggered once file is prduced successfully (if needed cooment it out)
               merge_shell[Era][region][channel][process].write('rm {}/*slim_*_{}_{}_{}_*_{}*.err\n'.format(farm_dir, Era, region, channel, process))
               merge_shell[Era][region][channel][process].close()
 
