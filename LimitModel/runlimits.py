b89a728cf74d1e6894ec034c3f1e8e7eb2541eae
AlbertHsuNTUphys <b07202037@ntu.edu.tw>
2024-11-08 14:51:21 +0100

    [Limit] improve signal xsec display

--- LimitModel/runlimits.py
+++ LimitModel/runlimits.py
@@ -21,7 +21,7 @@ parser.add_argument("--rtt", dest="rtt", default=0.6, type=float)
 parser.add_argument("--Masses",help='List of masses point. Default list=[200,300,350,400,500,600,700]',default=[200, 300, 350, 400, 500, 600, 700],nargs='+')
 parser.add_argument("--plot_only",help='Plot Only',action="store_true")
 parser.add_argument("--plot_y_max",help='Plot Only',default=1000,type=float)
-parser.add_argument("--plot_y_min",help='Plot Only',default=0.1,type=float)
+parser.add_argument("--plot_y_min",help='Plot Only',default=0.01,type=float)
 parser.add_argument("--datacard_dir", help='datacard directory', default='datacards_test', type=str)
 parser.add_argument("--outputdir",help='Create your favour outputdir. (If the directory is already existed, then the plots will simply stored under this directory, otherwise create one.)',default='./')
 parser.add_argument("--reset_outputfiles",help='Reset the output files.',action="store_true")
@@ -37,7 +37,8 @@ parser.add_argument('--signal_xsec', action='store_true')
 parser.add_argument('--sample_json', type=str, default='../data/sample.json')
 parser.add_argument('--Scan2D', action='store_true')
 parser.add_argument('--Scan2DNLL', action = 'store_true')
-parser.add_argument('--POI_name', type=str, default='r_2b')
+parser.add_argument('--POI_name', type=str, default='r_3b')
+parser.add_argument('--model_name', type=str, default='g2HDM_3Bbased')
 parser.add_argument('--ratio_file', type=str, default=None)
 args = parser.parse_args()
 
@@ -75,10 +76,36 @@ else:pass
 
 if args.plot_only:
 
+
+  signal_xsec_TGraph = None
+  if args.signal_xsec:
+    signal_xsec = array('d')
+    signal_xsec_up = array('d')
+    signal_xsec_do = array('d')
+    mass_bin    = array('d')
+    errx = array('d')
+    samples = Extend_sample_dict(read_json(args.sample_json), key_word='MASS')
+    for imass in mass_points:
+        mH = str(imass)
+        signal = signal_name_template.replace('MASS', mH)
+        xsec = samples[signal]['xsec'] if signal in samples else 0.0
+        xsec_err = samples[signal]['xsec_err'] if (signal in samples and 'xsec_err' in samples[signal]) else 10.0 #TODO: should be corrected
+        signal_xsec.append(xsec)
+        signal_xsec_up.append(xsec*xsec_err/100.)
+        signal_xsec_do.append(xsec*xsec_err/100.)
+        mass_bin.append(float(mH))
+        errx.append(0.0)
+    print(mass_bin)
+    print(signal_xsec)
+    signal_xsec_TGraph = ROOT.TGraphAsymmErrors(len(mass_bin), mass_bin, signal_xsec, errx, errx, signal_xsec_up, signal_xsec_do)
+
+
   if args.Scan2DNLL:
       for imass in mass_points:
           mH = str(imass)
-          RL.Save2DNLL(outputdir = args.outputdir,mass_point=Higgs_Mass_Name+str(imass), POI_name = args.POI_name)
+          RL.Save2DNLL(outputdir = args.outputdir,mass_point=Higgs_Mass_Name+str(imass), POI_name = args.POI_name, model_name = args.model_name, ratio_file = args.ratio_file)
+
+ 
 
   elif args.Scan2D:
     limitlog = RL.limitlog
@@ -91,38 +118,17 @@ if args.plot_only:
       TGraph_File_dict[Rb] = TGraph_File
       Rb_list.append(Rb)
       RL.limit_pdf_file = "limits_" + RL.analysis_ + "_"+ RL.signal_str_ + "_" + RL.postfix_+"_"+RL.model_ + "_" + RL.region_ + "_" + RL.channel_ + "_Rb%.1f.pdf"%(Rb)
-      RL.SaveLimitPdf1D(outputdir=args.outputdir, y_max=args.plot_y_max, y_min=args.plot_y_min)
+      print("saving")
+      RL.SaveLimitPdf1D(outputdir=args.outputdir, y_max=args.plot_y_max, y_min=args.plot_y_min, signal_xsec_TGraph=signal_xsec_TGraph)
 
     print(TGraph_File_dict)
-    Plot_2D_Limit_For(TGraph_File_dict, args.unblind, args.year, args.channel, args.outputdir, args.Masses, y_axis_title = 'R_{b}', ratio_file = args.ratio_file)
+    Plot_2D_Limit_For(TGraph_File_dict, args.unblind, args.year, args.channel, args.outputdir, args.Masses, y_axis_title = 'R_{b}', ratio_file = args.ratio_file, signal_xsec_TGraph = signal_xsec_TGraph)
     Plot_1D_Limit_For(TGraph_File_dict, args.unblind, y_max=args.plot_y_max, y_min=args.plot_y_min, year=[args.year], region=[args.region], channel=[args.channel], outputFolder=args.outputdir, Masses=args.Masses, mode = "Rb", legend_dict={(0.1*0):'pp\\rightarrow bH^{+}', (0.1*10):'pp\\rightarrow bH^{+} + H^{+}'}, AN=True)
   else:
     TGraph_File = RL.TextFileToRootGraphs(Masses=mass_points, Higgs=Higgs_Mass_Name)
     CheckDir(args.outputdir,True)
-    RL.SaveLimitPdf1D(outputdir=args.outputdir,y_max=args.plot_y_max,y_min=args.plot_y_min)
-    signal_xsec_TGraph = None
-    if args.signal_xsec:
-      signal_xsec = array('d')
-      signal_xsec_up = array('d')
-      signal_xsec_do = array('d')
-      mass_bin    = array('d')
-      errx = array('d')
-      samples = Extend_sample_dict(read_json(args.sample_json), key_word='MASS')
-      for imass in mass_points:
-        mH = str(imass)
-        signal = signal_name_template.replace('MASS', mH)
-        xsec = samples[signal]['xsec'] if signal in samples else 0.0
-        xsec_err = samples[signal]['xsec_err'] if (signal in samples and 'xsec_err' in samples[signal]) else 10.0 #TODO: should be corrected
-        signal_xsec.append(xsec)
-        signal_xsec_up.append(xsec*xsec_err/100.)
-        signal_xsec_do.append(xsec*xsec_err/100.)
-        mass_bin.append(float(mH))
-        errx.append(0.0)
-      print(mass_bin)
-      print(signal_xsec)
-      signal_xsec_TGraph = ROOT.TGraphAsymmErrors(len(mass_bin), mass_bin, signal_xsec, errx, errx, signal_xsec_up, signal_xsec_do)
-
-      RL.SaveLimitPdf1D(outputdir=args.outputdir,y_max=args.plot_y_max,y_min=args.plot_y_min, signal_xsec_TGraph=signal_xsec_TGraph)
+    #RL.SaveLimitPdf1D(outputdir=args.outputdir,y_max=args.plot_y_max,y_min=args.plot_y_min)
+    RL.SaveLimitPdf1D(outputdir=args.outputdir,y_max=args.plot_y_max,y_min=args.plot_y_min, signal_xsec_TGraph=signal_xsec_TGraph)
 else:
     counter=0
     template_card = "{dc_dir}/{year}/{signal}/{signal}_{year}_{region}_{channel}.txt".format(dc_dir=args.datacard_dir, year=year, signal=signal_name_template, region=region, channel=channel)
@@ -131,7 +137,7 @@ else:
         mH = str(imass)
         card_name = template_card.replace("MASS", mH)
         if args.Scan2DNLL:
-          RL.Scan2DNLL(card_name.replace('txt','root'), POI_name = args.POI_name, asimov=True, mass_point=Higgs_Mass_Name+str(imass), dc_dir=args.datacard_dir, out_dir=os.path.join(args.outputdir, '2DNLL'))
+          RL.Scan2DNLL(card_name.replace('txt','root'), POI_name = args.POI_name, asimov=True, mass_point=Higgs_Mass_Name+str(imass), dc_dir=args.datacard_dir, out_dir=os.path.join(args.outputdir, '2DNLL'), model_name = args.model_name)
 
         elif not args.Scan2D:
           logname = RL.getLimits(card_name,asimov=False, mass_point=Higgs_Mass_Name+str(imass),cminDefaultMinimizerStrategy=args.cminDefaultMinimizerStrategy, rAbsAcc=args.rAbsAcc, cminDefaultMinimizerTolerance=args.cminDefaultMinimizerTolerance, dc_dir=args.datacard_dir, log_dir='datacard_log')
