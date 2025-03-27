429aaaac479a2ab96c5a462f9f86cb6ad1254f32
Gouranga Kole <gouranga.tifr@gmail.com>
2025-03-27 11:13:20 +0100

    added extraCommand for runlimit

--- LimitModel/runlimits.py
+++ LimitModel/runlimits.py
@@ -122,6 +122,7 @@ parser.add_argument('--ratio_file', type=str, default=None)
 parser.add_argument('--coupling_varied', type=str, default='best_rtt_rtc')
 parser.add_argument('--all_signal', action = 'store_true')
 parser.add_argument('--fastScan', action = 'store_true')
+parser.add_argument("--extraCommand", help='extra addtional command', default='', type=str)
 args = parser.parse_args()
 
 year     = args.year
@@ -326,7 +327,7 @@ else:
           significance_log_file = RL.LogToSignificanceList(logname, param_list, 'w')
 
         else:
-          logname = RL.getLimits(card_name,asimov=False, mass_point=Higgs_Mass_Name+str(imass),cminDefaultMinimizerStrategy=args.cminDefaultMinimizerStrategy, rAbsAcc=args.rAbsAcc, cminDefaultMinimizerTolerance=args.cminDefaultMinimizerTolerance, dc_dir=args.datacard_dir, log_dir='datacard_log')
+          logname = RL.getLimits(card_name,asimov=False, mass_point=Higgs_Mass_Name+str(imass),cminDefaultMinimizerStrategy=args.cminDefaultMinimizerStrategy, rAbsAcc=args.rAbsAcc, cminDefaultMinimizerTolerance=args.cminDefaultMinimizerTolerance, dc_dir=args.datacard_dir, log_dir='datacard_log', extraCommand = extraCommand)
           mode_ = "w"
 
           if counter==0: mode_="w"
